870e805524f59b04d71ddcc36b999912e105109a
peach-junAi <jun-j6@jcom.home.ne.jp>
2015-08-28 11:41:58 +0900

    Delete mixi-support.meta.js

--- mixi-support.meta.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name           sengokuixa-meta-mixi-support
-// @description    戦国IXA-metaをmixiから使えるといいな
-// @version        0.0.0.1
-// @namespace      sengokuixa-meta
-// @include        http://*.sengokuixa.jp/*
-// @updateURL      https://raw.githubusercontent.com/moonlit-g/sengokuixa-meta/master/mixi-support.meta.js
-// ==/UserScript==
