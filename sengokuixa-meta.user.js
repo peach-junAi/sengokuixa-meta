cf0383cba6c2ef3535d6060b22a6d953395405c6
peach-junAi <jun-j6@jcom.home.ne.jp>
2015-08-30 17:22:03 +0900

    Update sengokuixa-meta.user.js

--- sengokuixa-meta.user.js
+++ sengokuixa-meta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           sengokuixa-meta
 // @description    戦国IXAを変態させるツール
-// @version        1.5.0.0
+// @version        1.5.0.1
 // @namespace      sengokuixa-meta
 // @include        http://*.sengokuixa.jp/*
 // @exclude        http://h*.sengokuixa.jp/*
